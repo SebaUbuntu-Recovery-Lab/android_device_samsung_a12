9d7e5e10d145854e2975592d342e0ffd35491059
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-03 08:52:35 +0100

    Update and rename omni_a12.mk to twrp_a12.mk

--- twrp_a12.mk
+++ twrp_a12.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/samsung/a12/device.mk)
 
 PRODUCT_DEVICE := a12
-PRODUCT_NAME := omni_a12
+PRODUCT_NAME := twrp_a12
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A125F
 PRODUCT_MANUFACTURER := samsung
